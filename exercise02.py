6dc30762a6947f69f891f9ad0b3b7038c2af3eb5
Mark Peters <mapeters101@earthlink.net>
2025-03-13 18:49:37 -0400

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,9 @@
+'''
+Question 2
+Write a function that, given a dataframe sructured as the one we created in
+Question 1 and a symbol name as a string (e.g. AAPL, MSFT, etc), will:
+
+return a similarly structured dataframe consisting of the row (or rows)
+containing the records with the highest volume for the given symbol
+raises a ValueError if the symbol is not in the dataframe
+'''

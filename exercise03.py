844e43cfd36ebfc4ed5c075ec74c52454066dd72
Mark Peters <mapeters101@earthlink.net>
2025-03-13 18:50:25 -0400

    Description for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,9 @@
+'''
+Question 3
+Using the same dataframe as in the preceding questions, our goal now is to
+write a function that will return, for a specific symbol, the row that had the
+largest high-low spread.
+
+Write a function to do that - it should just return a dataframe with the row
+(or rows) with the largest high-low spread.
+'''

b37922a046ecd6fb14294599298adcaa66dc5c07
Mark Peters <mapeters101@earthlink.net>
2025-03-13 18:51:17 -0400

    Description for exercise04.

---
+++ exercise04.py
@@ -0,0 +1,11 @@
+'''
+Question 4
+Using the same dataframe as the preceding questions, write a function that
+returns a single dataframe containing the record(s) with maximum high-low
+spread for each symbol in the dataframe. (Do not hardcode symbol names in this
+function - instead you should recover the possible symbol names from the data
+itself).
+
+The returned dataframe should have the same structure as the original
+dataframe, but just contain the rows of maximum high-low spread for each symbol.
+'''
